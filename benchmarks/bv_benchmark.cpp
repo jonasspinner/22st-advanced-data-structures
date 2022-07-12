9b7c68a90b03a8c70c7fb859aae3f942b5bca47e
Jonas Spinner <jonas.spinner@student.kit.edu>
2022-07-12 12:08:39 +0200

    Modify benchmark leaf sizes

--- benchmarks/bv_benchmark.cpp
+++ benchmarks/bv_benchmark.cpp
@@ -6,10 +6,10 @@
 #include "DynamicBitVector.h"
 
 
-template <class BV>
-static void BM_InsertN(benchmark::State& state) {
+template<class BV>
+static void BM_InsertN(benchmark::State &state) {
     size_t n = state.range(0);
-    for (auto _ : state) {
+    for (auto _: state) {
         std::minstd_rand gen;
         std::bernoulli_distribution bit_dist;
 
@@ -23,16 +23,16 @@ static void BM_InsertN(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BM_InsertN<ads::NaiveDynamicBitVector>)->Arg(1 << 5)->Arg(1 << 10)->Arg(1 << 15);
-BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint8_t, 2>>)->Arg(1 << 5)->Arg(1 << 10)->Arg(1 << 15);
-BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 2>>)->Arg(1 << 5)->Arg(1 << 10)->Arg(1 << 15);
-BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 4>>)->Arg(1 << 5)->Arg(1 << 10)->Arg(1 << 15);
-BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 8>>)->Arg(1 << 5)->Arg(1 << 10)->Arg(1 << 15);
-BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 16>>)->Arg(1 << 5)->Arg(1 << 10)->Arg(1 << 15);
+BENCHMARK(BM_InsertN<ads::NaiveDynamicBitVector>)->Arg(1 << 5)->Arg(1024)->Arg(1 << 15);
+BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint8_t, 2>>)->Arg(1 << 5)->Arg(1024)->Arg(1 << 15);
+BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 2>>)->Arg(1 << 5)->Arg(1024)->Arg(1 << 15);
+BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 4>>)->Arg(1 << 5)->Arg(1024)->Arg(1 << 15);
+BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 16>>)->Arg(1 << 5)->Arg(1024)->Arg(1 << 15);
+BENCHMARK(BM_InsertN<ads::DynamicBitVector<uint64_t, 64>>)->Arg(1 << 5)->Arg(1024)->Arg(1 << 15);
 
 
-template <class BV>
-static void BM_Insert(benchmark::State& state) {
+template<class BV>
+static void BM_Insert(benchmark::State &state) {
     int n = state.range(0);
     std::minstd_rand gen;
     std::bernoulli_distribution bit_dist;
@@ -42,32 +42,32 @@ static void BM_Insert(benchmark::State& state) {
         std::uniform_int_distribution<size_t> index_dist(0, bv.size());
         bv.insert(index_dist(gen), bit_dist(gen));
     }
-    for (auto _ : state) {
+    for (auto _: state) {
         std::uniform_int_distribution<int> index_dist(0, bv.size());
         bv.insert(index_dist(gen), bit_dist(gen));
         if (bv.size() == n + 128) {
             state.PauseTiming();
             for (int i = 0; i < 128; ++i) {
-                bv.remove(std::clamp(index_dist(gen), 0, n-1));
+                bv.remove(std::clamp(index_dist(gen), 0, n - 1));
             }
             state.ResumeTiming();
         }
     }
 }
-BENCHMARK(BM_Insert<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1<<15);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint8_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 8>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 32>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<24);
+
+BENCHMARK(BM_Insert<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1 << 15);
+//BENCHMARK(BM_Insert<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Insert<ads::DynamicBitVector<uint8_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Insert<ads::DynamicBitVector<uint64_t, 1024>>)->RangeMultiplier(2)->Range(8, 1 << 24);
 
 
-template <class BV>
-static void BM_Remove(benchmark::State& state) {
+template<class BV>
+static void BM_Remove(benchmark::State &state) {
     size_t n = state.range(0);
     std::minstd_rand gen;
     std::bernoulli_distribution bit_dist;
@@ -77,7 +77,7 @@ static void BM_Remove(benchmark::State& state) {
         std::uniform_int_distribution<size_t> index_dist(0, bv.size());
         bv.insert(index_dist(gen), bit_dist(gen));
     }
-    for (auto _ : state) {
+    for (auto _: state) {
         std::uniform_int_distribution<size_t> index_dist(0, bv.size());
         if (bv.size() == n) {
             state.PauseTiming();
@@ -89,21 +89,20 @@ static void BM_Remove(benchmark::State& state) {
         bv.remove(index_dist(gen));
     }
 }
-BENCHMARK(BM_Remove<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1<<15);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint8_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 8>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 32>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<24);
+
+BENCHMARK(BM_Remove<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1 << 15);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint8_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Remove<ads::DynamicBitVector<uint64_t, 1024>>)->RangeMultiplier(2)->Range(8, 1 << 24);
 
 
-
-template <class BV>
-static void BM_RemoveMid(benchmark::State& state) {
+template<class BV>
+static void BM_RemoveMid(benchmark::State &state) {
     size_t n = state.range(0);
     std::minstd_rand gen;
     std::bernoulli_distribution bit_dist;
@@ -113,7 +112,7 @@ static void BM_RemoveMid(benchmark::State& state) {
         std::uniform_int_distribution<size_t> index_dist(0, bv.size());
         bv.insert(index_dist(gen), bit_dist(gen));
     }
-    for (auto _ : state) {
+    for (auto _: state) {
         std::uniform_int_distribution<size_t> index_dist(0, bv.size());
         if (bv.size() == n) {
             state.PauseTiming();
@@ -125,19 +124,19 @@ static void BM_RemoveMid(benchmark::State& state) {
         bv.remove(bv.size() / 2);
     }
 }
-BENCHMARK(BM_RemoveMid<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1<<15);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint8_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 8>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 32>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<24);
 
-template <class BV>
-static void BM_Rank(benchmark::State& state) {
+BENCHMARK(BM_RemoveMid<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1 << 15);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint8_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_RemoveMid<ads::DynamicBitVector<uint64_t, 1024>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+
+template<class BV>
+static void BM_Rank(benchmark::State &state) {
     size_t n = state.range(0);
     std::minstd_rand gen;
     std::bernoulli_distribution bit_dist;
@@ -151,25 +150,24 @@ static void BM_Rank(benchmark::State& state) {
 
     std::uniform_int_distribution<size_t> index_dist(0, bv.size());
 
-    for (auto _ : state) {
+    for (auto _: state) {
         benchmark::DoNotOptimize(bv.rank(index_dist(gen), bit_dist(gen)));
     }
 }
 
-BENCHMARK(BM_Rank<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1<<15);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint8_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 8>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 32>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<24);
+BENCHMARK(BM_Rank<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1 << 15);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint8_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Rank<ads::DynamicBitVector<uint64_t, 1024>>)->RangeMultiplier(2)->Range(8, 1 << 24);
 
 
-template <class BV>
-static void BM_Select(benchmark::State& state) {
+template<class BV>
+static void BM_Select(benchmark::State &state) {
     size_t n = state.range(0);
     std::minstd_rand gen;
     std::bernoulli_distribution bit_dist;
@@ -184,20 +182,19 @@ static void BM_Select(benchmark::State& state) {
     auto num_ones = bv.rank(bv.size(), true);
     std::uniform_int_distribution<size_t> index_dist(1, num_ones);
 
-    for (auto _ : state) {
+    for (auto _: state) {
         benchmark::DoNotOptimize(bv.select(index_dist(gen), true));
     }
 }
 
-BENCHMARK(BM_Select<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1<<15);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint8_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1<<22);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 8>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 32>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1<<24);
-BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 128>>)->RangeMultiplier(2)->Range(8, 1<<24);
+BENCHMARK(BM_Select<ads::NaiveDynamicBitVector>)->RangeMultiplier(2)->Range(8, 1 << 15);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint8_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint8_t, 128>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 2>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 4>>)->RangeMultiplier(2)->Range(8, 1 << 22);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 16>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 64>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 256>>)->RangeMultiplier(2)->Range(8, 1 << 24);
+BENCHMARK(BM_Select<ads::DynamicBitVector<uint64_t, 1024>>)->RangeMultiplier(2)->Range(8, 1 << 24);
 
 BENCHMARK_MAIN();
