0140c0bd45a8a48e33542450183464936899956e
Jonas Spinner <jonas.spinner@student.kit.edu>
2022-07-13 09:48:58 +0200

    Modify data download script permissions

--- data/download_website_examples.sh
+++ data/download_website_examples.sh
@@ -6,7 +6,18 @@ for name in bv_10k\
             tree_d6_c5-10\
             tree_d8_c5;
 do
+  echo "example_${name}.txt"
   curl --request GET -sL \
        --url "https://algo2.iti.kit.edu/download/example_${name}.txt"\
        --output "example_${name}.txt"
+done
+
+for name in bv_10k\
+            bv_100k\
+            bv_1M;
+do
+  echo "example_${name}_output.txt"
+  curl --request GET -sL \
+       --url "https://algo2.iti.kit.edu/download/example_${name}_output.txt"\
+       --output "example_${name}_output.txt"
 done
